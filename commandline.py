62fa69816fc55c69977c1bfe661d1d5aca9386b7
Crcoli7307 <65417985+Crcoli7307@users.noreply.github.com>
2020-05-18 14:53:03 -0400

    Update commandline.py

--- commandline.py
+++ commandline.py
@@ -1,6 +1,22 @@
-print("Type Calculator to Open the Calculator or type Reboot to Reboot the script.")
+import datetime
+currentDT = datetime.datetime.now()
+print("Welcome to the command line! Make sure to type the name of the application exactly! For a list of apps, type: listapps")
+apps = "Notepad, Calculator, Time, Reboot"
 Command = input("> ")
-if Command == "Calculator":
+if Command == "Calculator" or Command == "calculator":
    exec(open("calculator.py").read())
-elif Command == "Reboot":
+elif Command == "Reboot" or Command == "reboot":
    exec(open("main.py").read())
+elif Command == "Notepad" or Command == "notepad":
+    exec(open("notepad.py").read())
+elif Command == "listapps":
+  print(apps)
+  exec(open("commandline.py").read())
+elif Command == "Time" or Command == "time":
+  print(str(currentDT))
+  exec(open("commandline.py").read())
+else:
+  print("A critical error has ocurred! Code: ERRINVCMD")
+  time.sleep(1)
+  print("Rebooting Command Line...")
+  exec(open("commandline.py").read())
