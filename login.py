e6b7e4028b96ff5e2e6294ca307ee25c50e53b2b
Crcoli7307 <65417985+Crcoli7307@users.noreply.github.com>
2020-05-18 14:51:49 -0400

    Update login.py

--- login.py
+++ login.py
@@ -1,5 +1,16 @@
+print("----LogIn Service----")
+print("Checking for saved user info...")
+#User Info Not Saveable in versions below V0.0.3
 Username = input("Create a Username:\n")
+if Username == "" or Username == " ":
+  print("Username can't be empty!")
+  exec(open("login.py").read())
 Password = input("Create a Password:\n")
+if Password == "" or Password == " ":
+  print("Password can't be empty!")
+  exec(open("login.py").read())
+print("Saving User Info...")
+print("User Info Not Saveable in versions below V0.0.3")
 print("Starting Main Boot...")
 print("Ⓦ ⓔ ⓛ ⓒ ⓞ ⓜ ⓔ   ⓣ ⓞ   Ⓟ ⓨ ⓣ ⓗ ⓞ ⓝ   Ⓞ Ⓢ !")
 print("Welcome,", Username,"to Python OS!")
